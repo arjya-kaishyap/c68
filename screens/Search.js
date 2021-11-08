2dcba49ea7f967cdc2f18b952b2739320b469767
Arjya Tust kashyap <80963174+arjya-kaishyap@users.noreply.github.com>
2021-11-08 06:59:11 -0400

    Add files via upload

---
+++ screens/Search.js
@@ -0,0 +1,25 @@
+import React from "react";
+import {Text, View, StyleSheet} from "react-native";
+
+export default class SearchScreen extends React.Component{
+
+    render() {
+        return(
+            <View style = {styles.container} >
+          <Text style = {styles.text}>Search Screen</Text>
+            </View>
+        )
+    }
+}
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "#5653D4"
+    },
+    text: {
+        color: "#ffff",
+        fontSize: 30
+    }
+})
